a76df8a99fc5bdb04bf55ea95c2c6df5fe706a3b
Bill Doughty <bdoughty@yahoo.com>
2008-12-02 10:46:02 -0600

    changed default root to accounts

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+map.root :controller => "accounts"
 
   # See how all your routes lay out with "rake routes"
 
